0b2db9efcb75aa853ef9928cb1fc56083d942b25
ridkhj <conserva.rdco@gmail.com>
2026-06-15 23:52:05 -0300

    feat: configura o app principal do express e middlewares globais

---
+++ server/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { logger } from './middleware/logger.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import musicaRoutes from './routes/musicaRoutes.js';
+import cantorRoutes from './routes/cantorRoutes.js';
+import repertorioRoutes from './routes/repertorioRoutes.js';
+import itemRepertorioRoutes from './routes/itemRepertorioRoutes.js';
+
+const app = express();
+
+
+app.use(express.json()); 
+app.use(logger);
+// Declaração das rotas das classes
+app.use('/api/musicas', musicaRoutes);
+app.use('/api/cantores', cantorRoutes);
+app.use('/api/repertorios', repertorioRoutes);
+app.use('/api/itens-repertorio', itemRepertorioRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+      api: 'Banco API',
+      versao: '1.0.0',
+      rotas: ['/musicas', '/cantores', '/repertorios', '/itens-repertorio'],
+    });
+});
+
+app.use(errorHandler);
+
+export default app;
